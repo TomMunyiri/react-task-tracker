0607cd75f7b7be2051884efe06ca17b996d8504b
tom <tmmunyiri@gmail.com>
2021-03-08 11:08:38 +0300

    add task

--- src/App.js
+++ src/App.js
@@ -28,6 +28,8 @@ function App() {
     },
   ]);
 
+  const [showAddTask, setShowAddTask] = useState(false)
+
   //delete a task
   //actions get passed up
   const deleteTask = (id) => {
@@ -39,10 +41,17 @@ function App() {
     setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
   }
 
+  //add task
+  const addTask=(task)=>{
+    const id=Math.floor(Math.random()*10000)+1
+    const newTask={id,...task}
+    setTasks([...tasks,newTask])
+  }
+
   return (
     <div className="container">
       <Header title='My Task Tracker' />
-      <AddTask/>
+      {showAddTask&&<AddTask addTask={addTask}/>}
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} toogleReminder={toogleReminder} />) : ("No tasks")}
     </div>
   );

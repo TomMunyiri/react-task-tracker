8d6436eec76d62a142520a89d46919b292f8d057
tom <tmmunyiri@gmail.com>
2021-03-08 04:03:16 +0300

    toggle reminder

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
   //toggle reminder
   const toogleReminder = (id) => {
-    console.log(id)
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
   }
 
   return (
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,7 @@
 import { FaTimes } from 'react-icons/fa'
 const Task = ({ task, onDelete, toogleReminder }) => {
     return (
-        <div className='task' onDoubleClick={() => toogleReminder(task.id)}>
+        <div className={`task ${task.reminder?'reminder':''}`} onDoubleClick={() => toogleReminder(task.id)}>
             <h3>{task.text}<FaTimes style={{ color: 'red', cursor: 'pointer' }}
                 onClick={() => onDelete(task.id)} /></h3>
             <p>{task.day}</p>
